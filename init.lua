b4d233decfb0206fac4997310f84b21b95257a5e
Alexander Novikov <kykybb29@gmail.com>
2024-04-13 12:11:27 +0300

    Update 13.04.2024

--- init.lua
+++ init.lua
@@ -1,7 +1,8 @@
 if vim.g.neovide == true then
-    vim.g.neovide_fullscreen = true
-    vim.o.guifont = "CodeNewRoman Nerd Font Mono:h14"
-    vim.g.neovide_scale_factor = 0.8
+    vim.g.neovide_fullscreen = false
+    vim.o.guifont = "CodeNewRoman Nerd Font Mono:h11"
+
+    vim.keymap.set("n", "<F11>", function() vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen end)
 end
 
 vim.g.mapleader = " "
@@ -13,17 +14,17 @@ vim.o.hlsearch = false
 
 vim.o.scrolloff = 5
 
-vim.o.tabstop = 4
+vim.o.tabstop = 2
 vim.o.expandtab = true
-vim.o.softtabstop = 4
-vim.o.shiftwidth = 4
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
 
 -- disable autro comments on new line
 vim.cmd([[autocmd FileType * set formatoptions-=ro]])
 
 -- Keymaps
-vim.keymap.set({"n", "v"}, "K", "{", { remap = true })
-vim.keymap.set({"n", "v"}, "J", "}", { remap = true })
+vim.keymap.set({"n", "v"}, "K", "5k", { remap = true })
+vim.keymap.set({"n", "v"}, "J", "5j", { remap = true })
 
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 vim.keymap.set("n", "<C-h>", "<C-w>h")
@@ -34,7 +35,9 @@ vim.keymap.set("n", "<leader>v", function() vim.cmd("vsplit") end)
 vim.keymap.set("n", "<leader>q", function() vim.cmd("q") end)
 
 vim.keymap.set({"n", "v"}, "<leader>y", "\"*y")
-vim.keymap.set({"n", "v"}, "<leader>p", "\"*p")
+vim.keymap.set({"n", "v"}, "<leader>p", "\"*p") 
+
+vim.keymap.set("n", "<leader>-", "70A-<Esc>70d|", { remap = true })
 
 -- Plugins
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
@@ -313,7 +316,7 @@ local cursor_background_color = "#282E36"
 local line_number_color = "#999999"
 local cursor_line_number_color = line_number_color
 local normal_color = "#eacda6"
-local comment_color = "#b34545"
+local comment_color = "#81796f"
 local string_color = "#81796f"
 local type_color = earth_yellow
 local identifier_color = normal_color
