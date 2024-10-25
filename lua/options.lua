2a4b7881268e981c8e6cf51b254246f862e22432
Alexander Novikov <kykybb29@gmail.com>
2024-10-25 08:05:22 +0300

    Added completion and other staff

--- lua/options.lua
+++ lua/options.lua
@@ -2,3 +2,13 @@ vim.o.expandtab = true
 vim.o.tabstop = 2
 vim.o.softtabstop = 2
 vim.o.shiftwidth = 2
+
+vim.wo.number = true
+vim.wo.relativenumber = true
+
+vim.o.scrolloff = 5
+
+vim.diagnostic.config = {
+  signs = false,
+  underline = false,
+}
